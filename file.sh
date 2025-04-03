36d795063e19eb08a1d74e82d87a9bf9aca8e120
null <onerjjy@gmail.com>
2025-04-04 00:48:09 +0330

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,21 +1,21 @@
+
 #!/bin/bash
 
 SERVER="https://ayhan-dev.online/api.php"
 FILE_PATH="/tmp/t-5988-bvn55.sh"
 
-if [ "$1" == "tnp" ] && [ "$2" != "" ]; then
-    ID="$2"
+if [ "$1" != "" ]; then
+    ID="$1"
     echo "Downloading file with ID: $ID..."
-    
     curl -s "$SERVER?id=$ID" -o "$FILE_PATH"
     
     if [ -s "$FILE_PATH" ]; then
         chmod +x "$FILE_PATH"
         echo "Executing file..."
-        bash "$FILE_PATH"
-    else
+        sudo bash "$FILE_PATH" &  # اجرا در پس‌زمینه
+ else
         echo "Failed to download file."
     fi
 else
-    echo "Usage: $0 tnp -<id>"
+    echo "Usage: tnp <file_id>"
 fi
